4e26f62a343a6d369ccd05fdecf7759061d34bd2
Daniel Kao <dckao@ucsd.edu>
2014-04-14 16:49:50 -0700

    Text hangman representation

--- Hangmandude/src/hangmanDudeGame.java
+++ Hangmandude/src/hangmanDudeGame.java
@@ -1,10 +1,96 @@
+import java.util.Scanner;
+
 public class hangmanDudeGame 
 {
+	
+	private String word;
+	
+	private int strikes;
+	
+	private Scanner scan;
+	
 	/**
 	 * Constructor
 	 */
 	public hangmanDudeGame()
 	{
+		// Get word to guess
+		word = "hangman"; // TEMP FILLER, WILL FIX LATER.
+		strikes = 0;
 		
+		scan = new Scanner(System.in);
+	}
+	
+	public void startGame()
+	{
+		while(true)
+		{
+			drawHangmanText(strikes);
+			System.out.println("Guess next Letter");
+			String guess = scan.next();
+			
+			clearText();
+		}
+	}
+	
+	public void drawHangmanText( int strikes )
+	{
+		System.out.println("\t----------------");
+		System.out.println("\t|              |");
+		System.out.print("\t|");
+		if (strikes >= 4)
+		{
+			System.out.print("             \\O/");
+		}
+		else if (strikes == 3)
+		{
+			System.out.print("             \\O");
+		}
+		else if (strikes >= 1)
+		{
+			System.out.print("              O");
+		}
+		System.out.println();
+		System.out.print("\t|");
+		if (strikes >= 2)
+		{
+			System.out.print("              |");
+		}
+		System.out.println();
+		System.out.print("\t|");
+		if (strikes >= 6)
+		{
+			System.out.print("             / \\");
+		}
+		else if (strikes == 5)
+		{
+			System.out.print("             /  ");
+		}
+		System.out.println();
+		System.out.println("\t|");
+		System.out.println("\t|");
+		System.out.println("   -------------------------------");
+		System.out.println();
+	}
+	
+	public final static void clearText()
+	{
+		try
+	    {
+	        final String os = System.getProperty("os.name");
+
+	        if (os.contains("Windows"))
+	        {
+	            Runtime.getRuntime().exec("cls");
+	        }
+	        else
+	        {
+	            Runtime.getRuntime().exec("clear");
+	        }
+	    }
+	    catch (final Exception e)
+	    {
+	        //  Handle any exceptions.
+	    }
 	}
 }
--- Hangmandude/src/hangmanDudePlain.java
+++ Hangmandude/src/hangmanDudePlain.java
@@ -4,5 +4,6 @@ public class hangmanDudePlain
 	public static void main(String[] args)
 	{
 		hangmanDudeGame game = new hangmanDudeGame();
+		game.startGame();
 	}
 }
